b46166bc8fd849a4124db55c7303d4352d17e1fa
Cole Brokamp <cole.brokamp@gmail.com>
2016-09-15 15:26:00 -0400

    don't need to set ports with new nginx-proxy setup

--- deployment_scripts/docker_shiny_run.sh
+++ deployment_scripts/docker_shiny_run.sh
@@ -14,12 +14,7 @@ VH=$1
 # set proxy env variables in shell and run image with them
 ssh -T $SERVER << HERE
       . ./proxy.sh
-      docker run -d --name $af -p 0:3838 -e http_proxy -e https_proxy -e VIRTUAL_HOST=$VH cole/${af}:latest
-      echo "cole/${af}:latest"
-      echo "running as"
-      echo "$af"
-      echo "on port"
-      docker port $af | sed -n 's/.*://p'
+      docker run -d --name $af -e http_proxy -e https_proxy -e VIRTUAL_HOST=$VH cole/${af}:latest
 HERE
 
 echo "proxied with the virtual host name"
